de42b0c45f58588d4f099a5df18b23ad0e793965
ZynxXW <152808095+ZynxXW@users.noreply.github.com>
2023-12-03 13:50:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,23 @@
-let robux = "";
-let fullRobux = "";
-chrome.storage.sync.get("fullrobux", function(items){
-	fullRobux = items["fullrobux"];
-})
+function updateElementFromStorage(storageKey, elementId) {
+  chrome.storage.sync.get(storageKey, function(items){
+    if (typeof items[storageKey] !== "undefined") {
+      let element = document.getElementById(elementId);
+      if (element) {
+        element.innerHTML = items[storageKey];
+      }
+    }
+  });
+}
 
-
-function OnRobuxMade() {
+function onRobuxMade() {
   if (document.getElementById('nav-robux-amount')) {
-   	chrome.storage.sync.get("robux", function(items){
-    		if (items["robux"] == undefined) return;
-		
-		element = document.getElementById("nav-robux-amount")
-
-		element.innerHTML = items["robux"];
-		
-		robux = items["robux"];
-		OnRobuxChange();
-	})
-  } 
-  else {
-    	setTimeout(OnRobuxMade, 5);
+    updateElementFromStorage("robux", "nav-robux-amount");
+    // Other logic...
+  } else {
+    setTimeout(onRobuxMade, 5);
   }
 }
 
-function OnRobuxChange() {
-	if (document.getElementById('nav-robux-amount').innerHTML != robux){
-		document.getElementById('nav-robux-amount').innerHTML = robux;
-		OnRobuxChange();
-	}
-	else {
-    		setTimeout(OnRobuxChange, 1);
-	}
+// Other functions...
 
-}
-function OnFullShow() {
-	if (document.getElementById('nav-robux-balance') != null && 
-	document.getElementById('nav-robux-balance').innerHTML != `<span>${fullRobux} Robux</span>`){
-		element = document.getElementById('nav-robux-balance');
-		element.innerHTML = `<span>${fullRobux} Robux</span>`;
-		element.title = fullRobux;
-		element.count = fullRobux;
-
-		OnFullShow();
-	}
-	else {
-		setTimeout(OnFullShow, 1);
-	}
-}
-
-OnRobuxMade();
-OnFullShow();
+onRobuxMade();
