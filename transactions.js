0361e88de8752cff2b503046b75f4038d724ad04
ZynxXW <152808095+ZynxXW@users.noreply.github.com>
2023-12-03 14:35:48 -0500

    Update transactions.js

--- transactions.js
+++ transactions.js
@@ -1,43 +1,41 @@
-// transactions.js
 let totalStr = "";
 let loaded = false;
 
 chrome.storage.sync.get(["fullrobux", "incoming", "pending", "bought"], function (items) {
-    let fullRobux = items.fullrobux;
+    let fullRobux = items.fullrobux || "";
 
     function setTotal() {
         let total = 0;
         const elements = document.getElementsByClassName("amount icon-robux-container");
-        for (let i = 0; i < 6; i++) {
-            if (elements[i]) {
-                total += parseInt(elements[i].children[2].textContent.split('.').join("").split(",").join(""));
-            }
+        for (let i = 0; i < elements.length && i < 6; i++) {
+            const amountText = elements[i]?.children[2]?.textContent || "0";
+            total += parseInt(amountText.split('.').join("").split(",").join(""));
         }
         totalStr = total.toLocaleString("en-150");
         loaded = true;
-        if (elements[7]) {
-            elements[7].children[2].textContent = totalStr;
+        const totalElement = elements[7]?.children[2];
+        if (totalElement && totalElement.textContent !== totalStr) {
+            totalElement.textContent = totalStr;
         }
     }
 
     function updateElements() {
         const balanceLabel = document.getElementsByClassName("balance-label icon-robux-container")[0];
-        const amountElements = document.getElementsByClassName("amount icon-robux-container");
-
-        if (balanceLabel && balanceLabel.children[0].textContent !== `<span class="shimmer-line"></span>`) {
+        if (balanceLabel && !balanceLabel.innerHTML.includes(fullRobux)) {
             balanceLabel.children[0].innerHTML = `My Balance: <span class="icon-robux-16x16"></span>${fullRobux}`;
         }
 
+        const amountElements = document.getElementsByClassName("amount icon-robux-container");
         if (amountElements.length > 0) {
-            if (amountElements[2]) {
-                amountElements[2].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${items.incoming}</span>`;
-            }
-            if (amountElements[5]) {
-                amountElements[5].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${items.pending}</span>`;
-            }
-            if (amountElements[1]) {
-                amountElements[1].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${items.bought}</span>`;
-            }
+            // Update incoming, pending, and bought amounts
+            const incomingElement = amountElements[2];
+            const pendingElement = amountElements[5];
+            const boughtElement = amountElements[1];
+
+            if (incomingElement) incomingElement.innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${items.incoming || ''}</span>`;
+            if (pendingElement) pendingElement.innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${items.pending || ''}</span>`;
+            if (boughtElement) boughtElement.innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${items.bought || ''}</span>`;
+
             setTotal();
         }
     }
@@ -52,5 +50,5 @@ chrome.storage.sync.get(["fullrobux", "incoming", "pending", "bought"], function
     });
 
     observer.observe(document, { childList: true, subtree: true, characterData: true });
-    updateElements();
+    updateElements(); // Initial update
 });
