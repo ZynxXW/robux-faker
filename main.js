6009c99f0955fb7fdb025f46865875706132af24
ZynxXW <152808095+ZynxXW@users.noreply.github.com>
2023-12-03 13:53:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
-function updateElementFromStorage(storageKey, elementId) {
-  chrome.storage.sync.get(storageKey, function(items){
-    if (typeof items[storageKey] !== "undefined") {
-      let element = document.getElementById(elementId);
-      if (element) {
-        element.innerHTML = items[storageKey];
-      }
-    }
-  });
+let robux = "";
+let fullRobux = "";
+
+// Function to update an element's innerHTML from Chrome storage
+function updateElementFromStorage(storageKey, elementId, callback) {
+    chrome.storage.sync.get(storageKey, function(items) {
+        if (typeof items[storageKey] !== "undefined") {
+            let element = document.getElementById(elementId);
+            if (element) {
+                element.innerHTML = items[storageKey];
+                if (callback) {
+                    callback();
+                }
+            }
+        }
+    });
 }
 
+// Check and update Robux balance
 function onRobuxMade() {
-  if (document.getElementById('nav-robux-amount')) {
-    updateElementFromStorage("robux", "nav-robux-amount");
-    // Other logic...
-  } else {
-    setTimeout(onRobuxMade, 5);
-  }
+    if (document.getElementById('nav-robux-amount')) {
+        updateElementFromStorage("robux", "nav-robux-amount", onRobuxChange);
+    } else {
+        setTimeout(onRobuxMade, 5);
+    }
 }
 
-// Other functions...
+// Monitor changes in Robux balance
+function onRobuxChange() {
+    let robuxElement = document.getElementById('nav-robux-amount');
+    if (robuxElement && robuxElement.innerHTML !== robux) {
+        robuxElement.innerHTML = robux;
+        setTimeout(onRobuxChange, 1);
+    } else {
+        setTimeout(onRobuxChange, 1);
+    }
+}
 
+// Check and update full Robux display
+function onFullShow() {
+    let fullRobuxElement = document.getElementById('nav-robux-balance');
+    if (fullRobuxElement && fullRobuxElement.innerHTML !== `<span>${fullRobux} Robux</span>`) {
+        fullRobuxElement.innerHTML = `<span>${fullRobux} Robux</span>`;
+        fullRobuxElement.title = fullRobux;
+        fullRobuxElement.count = fullRobux;
+        setTimeout(onFullShow, 1);
+    } else {
+        setTimeout(onFullShow, 1);
+    }
+}
+
+// Initial calls to start the functionality
 onRobuxMade();
+onFullShow();
