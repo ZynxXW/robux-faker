1aacec87a9bc31910e343e00f891d4feee738306
ZynxXW <152808095+ZynxXW@users.noreply.github.com>
2023-12-03 13:50:36 -0500

    Update transactions.js

--- transactions.js
+++ transactions.js
@@ -1,51 +1,57 @@
 let totalstr = "";
 let loaded = false;
-let fullRobux, incoming, pending, bought, total, element;
+let incoming = "";
+
+chrome.storage.sync.get("fullrobux", function(items){
+    let fullRobux = items["fullrobux"];
+})
 
 function setTotal() {
-    total = 0;
-    for (let i = 0; i < 6; i++){
-        if (element && element[i] && element[i].children[2]) {
-            total += parseInt(element[i].children[2].innerHTML.split('.').join("").split(",").join(""));
-        }
+    for (i=0; i<6; i++){
+        total += parseInt(element[i].children[2].innerHTML.split('.').join("").split(",").join(""))
+        if (i == 5)
+            continue;
     }
     totalstr = total.toLocaleString("en-150");
     loaded = true;
-    if (element && element[7] && element[7].children[2]) {
-        element[7].children[2].innerHTML = totalstr;
-    }
+    element[7].children[2].innerHTML = totalstr
 }
 
-function updateBalances() {
-    element = document.getElementsByClassName("amount icon-robux-container");
-    
-    if (element && element[2]) {
-        element[2].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${incoming}</span>`;
-    }
-    if (element && element[5]) {
-        element[5].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${pending}</span>`;
-    }
-    if (element && element[1]) {
-        element[1].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${bought}</span>`;
-    }
-    setTotal();
-}
 
-function retrieveDataAndSetup() {
-    chrome.storage.sync.get(["fullrobux", "incoming", "pending", "bought"], function(items){
-        fullRobux = items["fullrobux"];
-        incoming = items["incoming"];
-        pending = items["pending"];
-        bought = items["bought"];
-        updateBalances();
-    });
-}
-
-function OnBalanceExist() {
+function OnBalanceExist(){
     if (document.getElementsByClassName("balance-label icon-robux-container")[0]
     && document.getElementsByClassName("balance-label icon-robux-container")[0].children[0].innerHTML != `<span class="shimmer-line"></span>`){
         document.getElementsByClassName("balance-label icon-robux-container")[0].children[0].innerHTML = `My Balance: <span class="icon-robux-16x16"></span>${fullRobux}`;
-        retrieveDataAndSetup();
+        
+        chrome.storage.sync.get("incoming", function(items){
+            element = document.getElementsByClassName("amount icon-robux-container");
+            total = 0;
+        
+            incoming = items["incoming"];
+            element[2].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${incoming}</span>`;
+            
+            setTotal();
+        })
+
+        chrome.storage.sync.get("pending", function(items){
+            element = document.getElementsByClassName("amount icon-robux-container");
+            total = 0;
+        
+            pending = items["pending"];
+            element[5].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${pending}</span>`;
+            
+            setTotal();
+        })
+
+        chrome.storage.sync.get("bought", function(items){
+            element = document.getElementsByClassName("amount icon-robux-container");
+            total = 0;
+        
+            bought = items["bought"];
+            element[1].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${bought}</span>`;
+            
+            setTotal();
+        })
     }
     else {
         setTimeout(OnBalanceExist, 1);
@@ -53,9 +59,16 @@ function OnBalanceExist() {
 }
 
 function OnBalanceChange(){
-    if (loaded && element && element[7] && element[7].children[2] && element[7].children[2].innerHTML != totalstr){
-        updateBalances();
+    if (loaded &&
+    document.getElementsByClassName("amount icon-robux-container")[7].children[2].innerHTML != totalstr){
+        total = 0;
+        element = document.getElementsByClassName("amount icon-robux-container");
+        
+        element[2].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${incoming}</span>`;
+            
+        setTotal()
     }
+    
     else {
         setTimeout(OnBalanceChange, 1);
     }
